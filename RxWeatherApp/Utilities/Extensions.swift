c9a126776a3cd1db9ef4c90289addf97ae63dd10
JiWon Lee <jiwon.swdev@gmail.com>
2021-04-29 23:48:23 +0900

    feat: kelvin to celsius method

---
+++ RxWeatherApp/Utilities/Extensions.swift
@@ -0,0 +1,14 @@
+//
+//  Extensions.swift
+//  RxWeatherApp
+//
+//  Created by 이지원 on 2021/04/29.
+//
+
+import Foundation
+
+extension Double {
+    func toCelsius() -> Double {
+        return self - 273.15
+    }
+}
