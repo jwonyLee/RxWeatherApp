9cbed015cddb3db2756690b914188a5ad07fdc3f
JiWon Lee <jiwon.swdev@gmail.com>
2021-04-29 22:43:36 +0900

    feat: ViewController 컴포넌트 매칭

--- RxWeatherApp/Controller/MainViewController.swift
+++ RxWeatherApp/Controller/MainViewController.swift
@@ -9,20 +9,19 @@ import UIKit
 
 class MainViewController: UIViewController {
 
+    // MARK:- IBOutlet
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var statusLabel: UILabel!
+    @IBOutlet weak var iconImageView: UIImageView!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var windLabel: UILabel!
+    @IBOutlet weak var waterLabel: UILabel!
+    @IBOutlet weak var cloudLabel: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
---
+++ RxWeatherApp/ViewModel/MainViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  MainViewModel.swift
+//  RxWeatherApp
+//
+//  Created by 이지원 on 2021/04/28.
+//
+
+import Foundation
+import RxSwift
+
+class MainViewModel {
+
+    var disposeBag = DisposeBag()
+
+}
