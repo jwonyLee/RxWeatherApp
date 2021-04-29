08293869c1fa18ab2354d08c6c2fab175366828a
JiWon Lee <jiwon.swdev@gmail.com>
2021-04-29 22:51:18 +0900

    feat: WeatherInfo 생성

---
+++ RxWeatherApp/ViewModel/WeatherInfo.swift
@@ -0,0 +1,17 @@
+//
+//  Weather.swift
+//  RxWeatherApp
+//
+//  Created by 이지원 on 2021/04/29.
+//
+
+import Foundation
+
+struct WeatherInfo {
+    var city: String
+    var status: String
+    var icon: String
+    var temp: Int
+    var humidity: Int
+    var cloud: Int
+}
